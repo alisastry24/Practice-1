e95a09d4365f66a56361fd4aa8334d03d9ac4cf2
alisastry24 <alisarose2007@gmail.com>
2024-06-21 10:21:13 -0700

    Initial Commit

---
+++ Practice 1/Practice_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_1App.swift
+//  Practice 1
+//
+//  Created by Alisa Strykowski on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Practice_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
